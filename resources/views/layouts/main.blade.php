fd9fba5d93ea8181c53ddb3b94c5ff8130dfee22
thegitcreator <capital.1995@mail.ru>
2023-08-03 16:28:33 +0400

    CRUD for categories

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -147,7 +147,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="pages/gallery.html" class="nav-link">
+                        <a href="{{ route('category.index') }}" class="nav-link">
                             <i class="nav-icon fas fa-th-list"></i>
                             <p>Категории товаров</p>
                         </a>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [IndexController::class, 'adminPanel'])->name('main.index');
+
+Route::prefix('/categories')->namespace('\App\Http\Controllers\Category')->group(function() {
+    Route::get('/', 'IndexController@index')->name('category.index');
+    Route::get('/create', 'CreateController@create')->name('category.create');
+    Route::post('/', 'StoreController@store')->name('category.store');
+    Route::get('/{category}/edit', 'EditController@edit')->name('category.edit');
+    Route::get('/{category}', 'ShowController@show')->name('category.show');
+    Route::patch('/{category}', 'UpdateController@update')->name('category.update');
+    Route::delete('/{category}', 'DeleteController@delete')->name('category.delete');
 });
 
-Route::get('/', [IndexController::class, 'adminPanel'])->name('main.index');
