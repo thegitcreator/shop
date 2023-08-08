969d2562a9eb4dbc47ced04fbd65fdd0331271ac
thegitcreator <capital.1995@mail.ru>
2023-08-08 15:07:07 +0400

    API

---
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\Product;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return 111111;
+    }
+}
